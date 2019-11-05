ec1ff604456f22ae1d014348e7758fe8fa476408
老郑 <850812952@qq.com>
2019-11-05 15:55:22 +0800

    初始化标准模板工程

---
+++ src/main/java/com/fuful/controllers/UesrsCtrls.java
@@ -0,0 +1,24 @@
+package com.fuful.controllers;
+
+
+import com.fuful.domain.Users;
+import com.fuful.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class UesrsCtrls {
+    @Autowired
+    private UsersService usersService;
+
+    @RequestMapping(value = "test.do",method = GET)
+    public String test(){
+        Users users = usersService.login("xiaomin","123456");
+        System.out.println(users.getUid());
+        return "redirect:/index.jsp";
+    }
+
+}
