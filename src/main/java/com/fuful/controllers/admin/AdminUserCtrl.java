8ebdac6dad1e831851ce1e5560e6d3be7c8ea413
孙瑞彬 <1291156784@qq.com>
2019-11-18 08:11:20 +0800

    测试更改

--- src/main/java/com/fuful/controllers/admin/AdminUserCtrl.java
+++ src/main/java/com/fuful/controllers/admin/AdminUserCtrl.java
@@ -22,6 +22,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @Controller
 public class AdminUserCtrl {
 
+    //tres
     @Autowired
     private UserService userService;
 
